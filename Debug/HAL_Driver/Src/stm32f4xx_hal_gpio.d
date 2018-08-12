a1fc85a1f805f9430c9f7ffbf457e4aca163cb7c
unknown <ansahd@me.com>
2018-08-12 21:00:42 +0100

    set up environment..builds initial source and flashes to target

---
+++ Debug/HAL_Driver/Src/stm32f4xx_hal_gpio.d
@@ -0,0 +1,188 @@
+HAL_Driver/Src/stm32f4xx_hal_gpio.o: \
+ ../HAL_Driver/Src/stm32f4xx_hal_gpio.c \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_conf.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/projects/BoxingAid/CMSIS/device/stm32f4xx.h \
+ C:/projects/BoxingAid/CMSIS/device/stm32f407xx.h \
+ C:/projects/BoxingAid/CMSIS/core/core_cm4.h \
+ C:/projects/BoxingAid/CMSIS/core/core_cmInstr.h \
+ C:/projects/BoxingAid/CMSIS/core/cmsis_gcc.h \
+ C:/projects/BoxingAid/CMSIS/core/core_cmFunc.h \
+ C:/projects/BoxingAid/CMSIS/core/core_cmSimd.h \
+ C:/projects/BoxingAid/CMSIS/device/system_stm32f4xx.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_can.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_cryp.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dac.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_sram.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_nor.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_nand.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pccard.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_hash.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rng.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_usart.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_irda.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_hcd.h
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_conf.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/projects/BoxingAid/CMSIS/device/stm32f4xx.h:
+
+C:/projects/BoxingAid/CMSIS/device/stm32f407xx.h:
+
+C:/projects/BoxingAid/CMSIS/core/core_cm4.h:
+
+C:/projects/BoxingAid/CMSIS/core/core_cmInstr.h:
+
+C:/projects/BoxingAid/CMSIS/core/cmsis_gcc.h:
+
+C:/projects/BoxingAid/CMSIS/core/core_cmFunc.h:
+
+C:/projects/BoxingAid/CMSIS/core/core_cmSimd.h:
+
+C:/projects/BoxingAid/CMSIS/device/system_stm32f4xx.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_can.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_cryp.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dac.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_sram.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_nor.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_nand.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pccard.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_hash.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rng.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_usart.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_irda.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+C:/projects/BoxingAid/HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
---
+++ src/main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+* @file    main.c
+* @author  David Ansah
+* @date    11/08/2018
+* @brief   Main.c of boxing aid
+*******************************************************************************/
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f407xx.h"
+#include "main.h"
+			
+
+int main(void)
+{
+    for(;;)
+    {
+        // do nothing
+    }
+    return 0U;
+}
